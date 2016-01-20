076d35d854e7292993b21590488cea4ce4c793d5
Hugo F <hforestier@live.fr>
2016-01-20 09:36:45 +0100

    (f)

--- morpion.js
+++ morpion.js
@@ -1,26 +1,22 @@
-var grille = [["-", "-", "-"],
-              ["-", "-", "-"],
-              ["-", "-", "-"]];
+var grille = ["-","-","-","-","-","-","-","-","-"];
 
-var winCombinations = [ [[0,0],[0,1],[0,2]],
-                        [[1,0],[1,1],[1,2]],
-                        [[2,0],[2,1],[2,2]],
-                        [[0,0],[1,0],[2,0]],
-                        [[0,1],[1,1],[2,1]],
-                        [[0,2],[1,2],[2,2]],
-                        [[0,0],[1,1],[2,2]],
-                        [[0,2],[1,1],[2,0]] ];
+var winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 4, 8], [0, 4, 8], [6, 4, 2]];
+
+
+var player = "X";
 
 afficheGrille();
 
 function afficheGrille(){
+  var cpt = 0;	
   var html = "<table>";
-  for(var i = 0; i < grille.length; i++){
+  for(var i = 0; i < 3; i++){
     html += "<tr>";
-    for (var j = 0; j < grille[i].length; j++){
-      html += "<td onclick='addSign("+i+", "+j+")'>";
-      html += grille[i][j];
+    for (var j = 0; j < 3; j++){
+      html += "<td onclick='addSign("+cpt+")'>";
+      html += grille[cpt];
       html += "</td>" ;
+      cpt ++;
     }
     html += "</tr>";
   }
@@ -28,17 +24,14 @@ function afficheGrille(){
   document.getElementById("grille").innerHTML = html;
 }
 
-function playAI(){
 
-}
-
-function probCase(i, j){
-  
-}
-
-
-function addSign(i, j){
-  grille[i][j] = "O";
+function addSign(position){
+  if(
+  grille[position] = "O";
   playAI();
   afficheGrille();
+}
+
+function Player(){
+	
 }
