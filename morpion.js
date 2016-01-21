ec6ea58e0693d436bb2c550474a2db9c92d3842f
Nozkrew <thomas.maire1@hotmail.fr>
2016-01-21 09:41:19 +0100

    Debut de la partie IA

--- morpion.js
+++ morpion.js
@@ -2,6 +2,7 @@ var grille = ["-","-","-","-","-","-","-","-","-"];
 
 var winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
 
+//Tableau des probabilité pour chaque case
 var probPossibilities = [];
 
 var player = "X";
@@ -12,21 +13,19 @@ afficheGrille();
 //Rempli le tableau de probabilité
 function initProb(){
     for(var i = 0; i < 9; i++){
-        probPossibilities[i] = getCombinationsByIndex(i);
+        probPossibilities[i] = getCombinationsByIndex(i).length;
     }
-    console.log(probPossibilities);
-
 }
 
 //Indique le nombre de combinaisons gagnantes avec l'index passé en param.
 function getCombinationsByIndex(index){
-    var cpt = 0;
+    var tab = [];
     for(var i = 0; i < 8; i++){
         if(winCombinations[i].indexOf(index) != -1){
-            cpt ++;
+            tab.push(winCombinations[i]);
         }
     }
-    return cpt;
+    return tab;
 }
 
 function checkWin(){
@@ -64,14 +63,24 @@ function afficheGrille(){
 
 
 function addSign(position){
+    //Verifie que la case est dispo
     if(grille[position] == "-"){
+        //Vérifie quel joueur joue
         if(player == "X"){
             grille[position] = "X";
         }
         else{
             grille[position] = "O";
         }
+
+        //Change de joueur
         switchPlayer();
+
+        //Refaire le tableau de probabilité pour l'IA
+        calculateProbabilities();
+        if(player == "O"){
+            actionIA();
+        }
     }
     else{
         console.log("Case prise");
@@ -91,4 +100,82 @@ function switchPlayer(){
     else{
         player = "X";
     }
+}
+
+function calculateProbabilities(){
+    for(var i=0; i < 9; i++){
+        //Si la case est prise, la prob est null
+        if(grille[i] != "-"){
+            probPossibilities[i] = NaN;
+        }
+        else{
+            var prob = getCombinationsByIndex(i);
+            var cpt = prob.length;
+            for(var j = 0; j < prob.length; j++){
+                for(var k=0; k<3; k++){
+                    if(grille[prob[j][k]] == "X"){
+                        cpt--;
+                        break;
+                    }
+                }
+            }
+            probPossibilities[i] = cpt;
+        }
+    }
+    console.log(probPossibilities);
+}
+
+function actionIA(){
+    var position = "";
+
+    mustAttack();
+
+        //Partie aléatoire
+        //Recupère le nombre le + grand
+        var max = 0;
+        for(var i=0; i<9; i++){
+            if(!isNaN(probPossibilities[i])){
+                if(probPossibilities[i] > max){
+                    max = probPossibilities[i];
+                }
+            }
+        }
+
+        var tab = [];
+        for(var i=0; i<9; i++){
+            if(probPossibilities[i] == max){
+                tab.push(i);
+            }
+        }
+
+        var rand = Math.floor((Math.random() * (tab.length - 1)) + 0);
+        position = tab[rand];
+
+
+    addSign(position);
+}
+
+function mustAttack(){
+
+    for(var i = 0; i < 8; i++){
+        var tab =[];
+        for(var j=0; j<3; j++){
+            tab[winCombinations[i][j]] = grille[winCombinations[i][j]];
+        }
+        console.log(tab);
+        //Compte le nombre d'occurence de X dans le tableau
+        if(countOccurence(tab, "X") == 2){
+
+        }
+    }
+}
+
+function countOccurence(tab, search){
+    var cpt = 0;
+    for(var k=0; k < tab.length; k++){
+        if(tab[k] == search){
+            cpt++;
+        }
+    }
+    return cpt;
 }
